66b7331564fdf48ba2bf8c62d5b55188c7faa800
徐泽宇 <xzy@9elephas.com>
2017-11-26 23:22:30 +0800

    配置 nginx + tomcat 的动静分离

--- docs/docker/redis/master/dockerRun.sh
+++
@@ -1,2 +0,0 @@
-docker build -t redis/master .
-docker run --name redis-master -v /home/docker/redis/master/data:/data -d -p 16379:6379  --restart=always  redis/master
--- docs/docker/redis/slave/dockeRun.sh
+++
@@ -1,2 +0,0 @@
-docker build -t redis/slave .
-docker run --name redis-slave-01 -v /home/docker/redis/slave/data-01:/data -d -p 26379:6379  --restart=always  redis/slave
